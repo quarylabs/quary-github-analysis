f52df7fc7ed398d9e18892f051066c0b9339b39d
Louis <louisjoecodes@gmail.com>
2024-02-12 15:38:22 -0800

    feat: deployment frequency

---
+++ models/metrics/deployment_frequency.sql
@@ -0,0 +1,22 @@
+WITH deployment_counts AS (
+  SELECT
+    updated_at AS deployment_date,
+    repository_name,
+    COUNT(*) AS deployments
+  FROM
+      q.stg_raw_workflow_runs
+  WHERE
+    conclusion = 'success'
+    AND head_branch = 'main'
+  GROUP BY
+    deployment_date, repository_name
+)
+
+SELECT
+  deployment_date,
+  repository_name,
+  deployments
+FROM
+  deployment_counts
+ORDER BY
+  deployment_date DESC, repository_name;
---
+++ models/staging/stg_raw_workflow_runs.sql
@@ -0,0 +1,15 @@
+SELECT
+    id,
+    created_at,
+    updated_at,
+    status,
+    conclusion,
+    event,
+    head_branch,
+    JSON_VALUE(repository, '$.name') AS repository_name,
+    JSON_VALUE(actor, '$.login') AS actor_login,
+    workflow_id,
+    JSON_VALUE(head_commit, '$.id') AS head_commit_id,
+    JSON_VALUE(head_commit, '$.timestamp') AS head_commit_timestamp,
+FROM
+    q.raw_workflow_runs
